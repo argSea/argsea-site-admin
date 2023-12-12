316f5d70de70b018c5d1ae5f2c30459d47a43418
firelink <barbatos@argsea.com>
2023-12-12 00:37:49 -0500

    Some changes

---
+++ src/components/LinkTypes.tsx
@@ -0,0 +1,10 @@
+export const getLinkTypeChoices = () => {
+  return [
+    { id: "youtube", name: "YouTube" },
+    { id: "appstore", name: "App Store" },
+    { id: "playstore", name: "Play Store" },
+    { id: "Other Repo", name: "Other Repo" },
+    { id: "website", name: "Website" },
+    { id: "other", name: "Other" },
+  ];
+};
--- src/components/ProjectEdit.tsx
+++ src/components/ProjectEdit.tsx
@@ -12,17 +12,19 @@ import {
   SimpleFormIterator,
   DateTimeInput,
   AutocompleteInput,
+  SelectInput,
 } from "react-admin";
 import { RichTextInput } from "ra-input-rich-text";
 import { getSkillChoices } from "./Skills";
 import { getRoleChoices } from "./Roles";
 import { getProjectTypeChoices } from "./ProjectTypes";
+import { getLinkTypeChoices } from "./LinkTypes";
 
 const ProjectEdit = (props: any) => {
   const skillChoices = getSkillChoices();
   return (
     <Edit title="Edit a project" {...props}>
-      <SimpleForm>
+      <SimpleForm sx={{ display: "flex" }}>
         <TextInput disabled source="id" />
         <ArrayInput source="userIDs" label="Associated UserIDs">
           <SimpleFormIterator>
@@ -39,24 +41,39 @@ const ProjectEdit = (props: any) => {
         <DateTimeInput source="updatedDate" defaultValue={new Date().toISOString()} />
         <DateTimeInput source="publishedDate" />
         <TextInput source="repoURL" label="Repo URL" />
+        <NumberInput source="priority" />
+        <BooleanInput source="isActive" />
+        <BooleanInput source="isReleased" />
+        <BooleanInput source="isHidden" />
+        <hr style={{ width: "100%" }} />
         <RichTextInput source="description" />
-        <ImageInput source="icon">
+        <AutocompleteArrayInput source="skills" autoHighlight={true} autoSelect={true} choices={skillChoices} ChipProps={{ color: "primary" }} />
+        <CheckboxGroupInput source="roles" choices={getRoleChoices()} />
+        {/* <ImageInput source="icon">
           <ImageField source="src" />
-        </ImageInput>
+        </ImageInput> */}
+        <ArrayInput source="links">
+          <SimpleFormIterator inline>
+            <SelectInput source="type" choices={getLinkTypeChoices()} />
+            <TextInput source="text" />
+            <TextInput source="url" label="URL" />
+          </SimpleFormIterator>
+        </ArrayInput>
+        <hr style={{ width: "100%" }} />
         <ArrayInput source="images">
           <SimpleFormIterator>
-            <TextInput source="order" />
+            <NumberInput source="order" />
             <ImageInput source="image">
               <ImageField source="src" />
             </ImageInput>
           </SimpleFormIterator>
         </ArrayInput>
-        <NumberInput source="priority" />
-        <BooleanInput source="isActive" />
-        <BooleanInput source="isReleased" />
-        <BooleanInput source="isHidden" />
-        <AutocompleteArrayInput source="skills" autoHighlight={true} autoSelect={true} choices={skillChoices} ChipProps={{ color: "primary" }} />
-        <CheckboxGroupInput source="roles" choices={getRoleChoices()} />
+        {/* <ArrayInput source="snippets">
+          <SimpleFormIterator inline>
+            <TextInput source="name" />
+            <TextInput source="code" multiline />
+          </SimpleFormIterator>
+        </ArrayInput> */}
       </SimpleForm>
     </Edit>
   );
